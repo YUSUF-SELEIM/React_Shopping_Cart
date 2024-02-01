b1c33d65db6a8b7e84b0f3800d9469dcb279af71
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:45:57 +0200

    here we are fetching the products and mapping each into a separate card buy now button is linked to the context of the chosen products

---
+++ src/Components/ShoppingPage/Products.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import LoadingSpinner from "../LoadingSpinner";
+import ErrorPage from "../ErrorPage";
+
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get("https://fakestoreapi.com/products?");
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching Products: " + error.message);
+  }
+};
+
+export default function Products({ handleAddChosenProductsInCart }) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["Products"],
+    queryFn: fetchProducts,
+  });
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <div className="flex flex-wrap gap-8 p-5 justify-center ">
+      {data.map((product) => (
+        <div
+          key={product.id}
+          className="mt-24 card w-80 bg-base-100 shadow-xl justify-between hover:transform hover:scale-105 transition-transform duration-300 ease-in-out"
+        >
+          <figure className="px-5 pt-5">
+            <img
+              src={product.image}
+              alt="Shoes"
+              className="rounded-xl max-w-[100%] h-[30vh]"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title w-60 truncate ">{product.title}</h2>
+            <p className="text-xl font-bold text-green-700">
+              {product.price} $
+            </p>
+            <div className="card-actions">
+              <button
+                className="btn btn-primary"
+                onClick={() => handleAddChosenProductsInCart(product)}
+              >
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
