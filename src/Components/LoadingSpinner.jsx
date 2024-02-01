e165775b5913c10d27d98e46c5e3a1208e865beb
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:52:06 +0200

    Separated the loadingSpinner in it's own component

---
+++ src/Components/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  );
+}
