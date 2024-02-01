742209231c9dcc85adad52a1b7e72c8bc5224749
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:42:46 +0200

    We Collect the ShoppingPage Components in here also here we are using the Context of the chosen products

--- src/Components/ShoppingPage/ShoppingPage.jsx
+++ src/Components/ShoppingPage/ShoppingPage.jsx
@@ -1,5 +1,21 @@
-export default function ShoppingPage(){
-    return(
-        <h1>Router</h1>
-    );
+import Navbar from "./Navbar";
+import Products from "./Products";
+import {useChosenProducts } from "../ChosenProductsProvider"; // Import your context
+
+
+export default function ShoppingPage() {
+    const { chosenProducts, addChosenProduct } = useChosenProducts();
+
+    const handleAddChosenProduct = (product) => {
+      addChosenProduct(product);
+    };
+
+  return (
+    <div>
+      <Navbar chosenProducts={chosenProducts}></Navbar>
+      <Products
+        handleAddChosenProductsInCart={handleAddChosenProduct}
+      ></Products>
+    </div>
+  );
 }
