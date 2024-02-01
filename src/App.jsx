3dafbfc2cf2ea16224391eda88ecd445abad170f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:55:36 +0200

    wrapped the context in here and called the separated components of error and loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import ShopWindow from "./Components/ShopWindow/ShopWindow";
 import { useQuery } from "@tanstack/react-query";
-
 import axios from "axios";
+import ShopWindow from "./Components/ShopWindow/ShopWindow";
+import LoadingSpinner from "./Components/LoadingSpinner";
+import ErrorPage from "./Components/ErrorPage";
+import { ChosenProductsProvider } from "./Components/ChosenProductsProvider"; // Import your context
 
 const fetchCarouselImages = async () => {
   try {
     const response = await axios.get(
-      "https://fakestoreapi.com/products?limit=5"
+      "https://fakestoreapi.com/products?limit=10"
     );
     return response.data;
   } catch (error) {
@@ -21,35 +23,19 @@ function App() {
   });
 
   if (isLoading) {
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    );
+    return <LoadingSpinner></LoadingSpinner>;
   }
 
   if (error) {
-    return (
-      <div className="h-screen">
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div className="text-center">
-            <p className="text-base font-semibold text-red-600">404</p>
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Network Error
-            </h1>
-            <p className="mt-6 text-base leading-7 text-gray-600">
-              Are you offline ?
-            </p>
-          </div>
-        </main>
-      </div>
-    );
+    return <ErrorPage></ErrorPage>;
   }
 
   return (
-    <div className="flex flex-col w-full ">
-      <ShopWindow carouselImages={data}></ShopWindow>
-    </div>
+    <ChosenProductsProvider>
+      <div className="flex flex-col w-full ">
+        <ShopWindow carouselImages={data}></ShopWindow>
+      </div>
+    </ChosenProductsProvider>
   );
 }
 
