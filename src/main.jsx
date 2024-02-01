86c570c0f1c1d8a1332211b648005d42517bcf8e
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 20:35:09 +0200

    Added the new routes of the ProductPage

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ShoppingPage from "./Components/ShoppingPage/ShoppingPage.jsx";
 import Cart from "./Components/Cart/Cart.jsx";
+import ProductPage from "./Components/ShoppingPage/ProductPage.jsx";
 import { ChosenProductsProvider } from "./Components/ChosenProductsProvider"; // Import your context
 
 const queryClient = new QueryClient({
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
     element: <ShoppingPage />,
   },
   {
+    path: "ProductPage/:id",
+    element: <ProductPage />,
+  },
+  {
     path: "/Cart",
     element: <Cart />,
   },
@@ -32,9 +37,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     {/* <React.StrictMode> */}
-      <ChosenProductsProvider>
-        <RouterProvider router={router} />
-      </ChosenProductsProvider>
+    <ChosenProductsProvider>
+      <RouterProvider router={router} />
+    </ChosenProductsProvider>
     {/* </React.StrictMode> */}
   </QueryClientProvider>
 );
