c4dc03adfba6ee5fb898e221db641208f3a50511
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 20:32:40 +0200

    Added a ProductPage for each Product from which an item could be added to the cart

---
+++ src/Components/ShoppingPage/ProductNavbar.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom";
+
+export default function Navbar({ chosenProducts }) {
+  const totalItems = Array.from(chosenProducts.values()).reduce(
+    (total, product) => {
+      return total + product.quantity;
+    },
+    0
+  );
+  const totalPrice = Array.from(chosenProducts.values()).reduce(
+    (total, product) => {
+      return total + product.price;
+    },
+    0
+  );
+  return (
+    <div className="navbar bg-gray-100 shadow-2xl justify-between">
+      <div className="navbar-center">
+        <button className="text-xl -skew-y-12 origin-bottom font-extrabold hover:skew-y-0">
+          <Link to={"/ShoppingPage"}>Garments</Link>
+        </button>
+      </div>
+      <div className="navbar-end">
+        <div className="flex space-x-2 px-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item">
+                  {totalItems}
+                </span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow-2xl "
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">{totalItems} Items</span>
+                <span className="text-info">
+                  total: ${parseInt(totalPrice)}
+                </span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    <Link to="/Cart">View cart</Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/ShoppingPage/ProductPage.jsx
@@ -0,0 +1,74 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Navbar from "./ProductNavbar";
+import LoadingSpinner from "../LoadingSpinner";
+import ErrorPage from "../ErrorPage";
+import { useChosenProducts } from "../ChosenProductsProvider";
+import Zoom from "react-img-zoom";
+
+const fetchSelectedProducts = async (id) => {
+  try {
+    const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching Products: " + error.message);
+  }
+};
+
+export default function ProductPage() {
+  const { chosenProducts, addChosenProduct } = useChosenProducts();
+  const { id } = useParams();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["SelectedProducts", id],
+    queryFn: () => fetchSelectedProducts(id),
+  });
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (error) {
+    return <ErrorPage />;
+  }
+
+  return (
+    <>
+      <Navbar chosenProducts={chosenProducts}></Navbar>
+      <div className="w-full flex flex-col items-center mt-10 h-[80vh] md:mt-0 md:flex-row md:items-center md:justify-center md:space-x-6">
+        <div className="cursor-zoom-in">
+          <Zoom img={data.image} zoomScale={2} width={600} height={400} />
+        </div>
+        <div className="h-[70%] flex flex-col justify-between">
+          <div>
+            <div className="text-3xl">{data.title}</div>
+            <div className="w-full flex space-x-8 py-4">
+              <div className=" text-sm">{data.rating.rate} ⭐</div>
+              <div className=" text-sm">
+                🛒 {data.rating.count} sold recently
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end px-6">
+            <div className="flex flex-col items-center space-y-6">
+              <div className="text-2xl font-bold text-green-700 ">
+                {data.price}
+                <span className="text-3xl">$</span>
+              </div>
+              <button
+                className="btn btn-accent"
+                onClick={() => addChosenProduct(data)}
+              >
+                Add To Cart
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="text-xl font-bold">Product Description</div>
+            <div className="text-sm">{data.description}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
