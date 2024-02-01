24e9358da7e179220c4850114e92a37d843d41bc
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:37:49 +0200

    Changed Text

--- src/Components/ShopWindow/ShopNow.jsx
+++ src/Components/ShopWindow/ShopNow.jsx
@@ -4,10 +4,10 @@ export default function ShopNow() {
   return (
     <div className="h-screen flex flex-col justify-center items-center space-y-14">
       <div className="text-7xl font-bold text-center">
-        Discover More Options
+        Discover More
       </div>
       <button className="btn btn-primary w-1/2">
-        <Link to="ShoppingPage">Shop Now</Link>
+        <Link to="/ShoppingPage">Shop Now</Link>
       </button>
     </div>
   );
