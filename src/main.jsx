f101a3b68db378a443bd8bdf3f52df329c375b61
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 12:54:46 +0200

    removed strict mode and added some routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ShoppingPage from "./Components/ShoppingPage/ShoppingPage.jsx"
-
+import ShoppingPage from "./Components/ShoppingPage/ShoppingPage.jsx";
+import Cart from "./Components/Cart/Cart.jsx";
+import { ChosenProductsProvider } from "./Components/ChosenProductsProvider"; // Import your context
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,14 +21,20 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "ShoppingPage",
+    path: "/ShoppingPage",
     element: <ShoppingPage />,
+  },
+  {
+    path: "/Cart",
+    element: <Cart />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-      <React.StrictMode>
-      <RouterProvider router={router} />
-      </React.StrictMode>
+    {/* <React.StrictMode> */}
+      <ChosenProductsProvider>
+        <RouterProvider router={router} />
+      </ChosenProductsProvider>
+    {/* </React.StrictMode> */}
   </QueryClientProvider>
 );
