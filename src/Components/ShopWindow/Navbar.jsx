99fdd36ec08d4f7184424bfa8f60267cfc435c36
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 20:28:12 +0200

    "Garments" routes to the Shop Window

--- src/Components/ShopWindow/Navbar.jsx
+++ src/Components/ShopWindow/Navbar.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="navbar bg-gray-100 fixed shadow-2xl z-10">
       <div className="navbar-center">
-        <a className="text-xl  -skew-y-12 origin-bottom font-extrabold">
-          Garments
-        </a>
+        <button className="text-xl -skew-y-12 origin-bottom font-extrabold hover:skew-y-0">
+          <Link to={"/"}>Garments</Link>
+        </button>
       </div>
     </div>
   );
