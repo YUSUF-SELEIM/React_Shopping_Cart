10debe77cf27315ceddbe39763dbc268c0201ae6
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-02-01 23:57:18 +0200

    made it mobile compatible adding an item to cart routes to the cart immediately

--- src/Components/ShoppingPage/ProductPage.jsx
+++ src/Components/ShoppingPage/ProductPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Navbar from "./ProductNavbar";
@@ -6,6 +6,7 @@ import LoadingSpinner from "../LoadingSpinner";
 import ErrorPage from "../ErrorPage";
 import { useChosenProducts } from "../ChosenProductsProvider";
 import Zoom from "react-img-zoom";
+import { useEffect, useState } from "react";
 
 const fetchSelectedProducts = async (id) => {
   try {
@@ -17,13 +18,29 @@ const fetchSelectedProducts = async (id) => {
 };
 
 export default function ProductPage() {
+  const [isMobile, setIsMobile] = useState(false);
   const { chosenProducts, addChosenProduct } = useChosenProducts();
   const { id } = useParams();
   const { data, isLoading, error } = useQuery({
     queryKey: ["SelectedProducts", id],
     queryFn: () => fetchSelectedProducts(id),
   });
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
 
+    // Set initial mobile state
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   if (isLoading) {
     return <LoadingSpinner />;
   }
@@ -36,9 +53,12 @@ export default function ProductPage() {
     <>
       <Navbar chosenProducts={chosenProducts}></Navbar>
       <div className="w-full px-4 flex flex-col items-center mt-10 h-[80vh] md:mt-0 md:flex-row md:items-center md:justify-center md:space-x-6">
-        <div className="cursor-zoom-in">
-          <Zoom img={data.image} zoomScale={2} width={600} height={400} />
-        </div>
+        {!isMobile && (
+          <div className="cursor-zoom-in">
+            <Zoom img={data.image} zoomScale={2} width={600} height={400} />
+          </div>
+        )}
+        {isMobile && <img src={data.image} className="h-[50%] py-4" alt="" />}
         <div className="h-[70%] flex flex-col justify-between">
           <div>
             <div className="text-3xl">{data.title}</div>
@@ -55,15 +75,17 @@ export default function ProductPage() {
                 {data.price}
                 <span className="text-3xl">$</span>
               </div>
-              <button
-                className="btn btn-accent"
-                onClick={() => addChosenProduct(data)}
-              >
-                Add To Cart
-              </button>
+              <Link to={"/Cart"}>
+                <button
+                  className="btn btn-accent"
+                  onClick={() => addChosenProduct(data)}
+                >
+                  Add To Cart
+                </button>
+              </Link>
             </div>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col py-4 md:py-0">
             <div className="text-xl font-bold">Product Description</div>
             <div className="text-sm">{data.description}</div>
           </div>
